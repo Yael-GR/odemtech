31be342a7e2963dfaa6958ed3773553104b97f0d
Yael-GR <130078623+Yael-GR@users.noreply.github.com>
2024-12-18 16:20:41 +0200

    Add comments to js file

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let redPoints = 0;
+let redPoints = 0; // Initialize points
 let bluePoints = 0;
 
-const btn = document.querySelector("#btn-container");
-console.log(btn);
+const btn = document.querySelector("#btn-container"); // Get DOM nodes
+const redPointsDisplay = document.querySelector("p.red")
+const bluePointsDisplay = document.querySelector("p.blue")
 
-btn.addEventListener("click", (event) => {
+btn.addEventListener("click", (event) => { // Add points on click
     addPoints(event.target);
 });
 
 function addPoints (button) {
     const pointsToAdd = getPoints(button);
     const team = getTeam(button);
-    if (team === "red") {redPoints += pointsToAdd}
-    else if (team === "blue") {bluePoints += pointsToAdd}
-    console.log(`Red: ${redPoints}`);
-    console.log(`Blue: ${bluePoints}`)
+    if (team === "red") {
+        redPoints += pointsToAdd;
+        redPointsDisplay.textContent = `Red team's score: ${redPoints}`; // Update score display
+    }
+    else if (team === "blue") {
+        bluePoints += pointsToAdd;
+        bluePointsDisplay.textContent = `Red team's score: ${bluePoints}`; // Update score display
+    }
 }
 
 function getTeam (button) {
@@ -24,9 +29,8 @@ function getTeam (button) {
 }
 
 function getPoints (button) {
-    const classArr = Array
-                    .from(button.classList)
-                    .filter(cls => cls.includes("points"));
-    const pointsArr = classArr[0].split("-")
-    return parseInt(pointsArr[1]);
+    const classArr = Array.from(button.classList) // Create array of the button's classes
+                    .filter(cls => cls.includes("points")); // Remove all classes except the points
+    const pointsArr = classArr[0].split("-") // Points classes are always "points-x]"
+    return parseInt(pointsArr[1]); // Gets the number from the class name
 }
